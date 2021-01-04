2607f600c70e9a831f3609a3102453345bfa5759
Sam Hirsch <sam.hirsch66@gmail.com>
2021-01-03 22:37:30 -0700

    Add chromiumInstallPath to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const baseUrl =
   process.env.THEPIRATEBAY_DEFAULT_ENDPOINT || "https://thepiratebay.org";
+export const chromiumInstallPath = process.env.CHROMIUM_INSTALL_PATH || "/usr/bin/chromium-browser";
